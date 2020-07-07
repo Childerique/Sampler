5a4be36025048d19e50534a8db44ff2f5ad4868d
Felix GENSOLLEN <gens.felix@gmail.com>
2020-07-07 13:38:34 +0200

    Correction GPL

--- SaveScript.py
+++ SaveScript.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-# This file is part of BnBSampler.
+# This file is part of Sampler.
 # 
-# BnBSampler is free software: you can redistribute it and/or modify
+# Sampler is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
 # 
-# BnBSampler is distributed in the hope that it will be useful,
+# Sampler is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
